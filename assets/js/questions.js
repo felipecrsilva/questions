7348247d224f384d3c5e8d002d46ed53829a2638
Felipe <felipecrs32@gmail.com>
2022-01-15 13:10:51 -0300

    Completed project

---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+const quiz = [
+  {
+      q: 'Jesus de Nazaré, o nosso Salvador, aonde Ele nasceu ?',
+      options: ['Nazaré','Belém','Pará','New York','São Paulo'],
+      answer: 1,
+  },
+  {
+      q: 'Quais discípulos perguntaram a Jesus se podiam fazer descer fogo do céu ?',
+      options: ['João e Tiago','Pedro e João','Tiago e Pedro'],
+      answer: 0,
+  },
+  {
+      q: 'Qual era o nome babilônico de Daniel ?',
+      options: ['Aspenaz','Abede-Nego','Beltessazar','Sadraque','Mesaque'],
+      answer: 2,
+  },
+  {
+      q: 'Qual o nome que Jacó deu ao lugar onde sonhou com Deus ?',
+      options: ['Betuel','Canaã','Sonho Divino','Luz','Betel'],
+      answer: 4,
+  },
+  {
+      q: 'De acordo, com a visão poética de João, qual era o número de cavaleiro(s) do Apocalipse ?',
+      options: ['7','4','5','1','2'],
+      answer: 1,
+  },
+  {
+      q: 'Quem teve o corpo disputado pelo Arcanjo Miguel e o Diabo ?',
+      options: ['Jesus','Judas','Moisés','João','Paulo'],
+      answer: 2,
+  },
+  {
+      q: 'Quantos filhos Jó perdeu ?',
+      options: ['10','9','7','12','4'],
+      answer: 0,
+  },
+  {
+      q: 'Quem foi o ou a pessoa responsável por fazer Sansão perder a força? ',
+      options: ['Dalila','Filisteu','Cananeu','Homem','Egípcius'],
+      answer: 3,
+  },
+  {
+      q: 'Quem era conhecido como o "Filho da Promessa" ?',
+      options: ['Isaque','Ismael','José','Jacó','Jesus'],
+      answer: 0,
+  },
+  {
+      q: 'Complete a frase: e disse: Nu saí do ventre de minha mãe e nu tornarei para lá; o ______ o deu e o ______ o tomou; bendito seja o nome do Senhor. Jó 1:21 ARC',
+      options: ['Deus','Jesus','Senhor'],
+      answer: 2,
+  },
+]
